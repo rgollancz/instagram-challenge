486662cb546784400946f40232b1b7c558e6ccb9
Rosie <r.gollancz@gmail.com>
2016-11-19 16:22:59 +0000

    Add redirect on Posts index when logged out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
   def index
+    if !user_signed_in?
+      redirect_to '/'
+    end
   end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    it "returns http success when logged in" do
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "redirects to home when logged out" do
+      get :index
+      expect(response).to have_http_status(:redirect)
     end
   end
 
